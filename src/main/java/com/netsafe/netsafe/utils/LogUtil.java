5f1c574a2abe1140f599f3a1abcce9c5c134927a
AYssu <2997036064@qq.com>
2024-03-17 20:54:21 +0800

    修复上线功能

--- src/main/java/com/netsafe/netsafe/utils/LogUtil.java
+++ src/main/java/com/netsafe/netsafe/utils/LogUtil.java
@@ -3,7 +3,7 @@ package com.netsafe.netsafe.utils;
 public class LogUtil {
     private static final String TAG = "netSafe";
 
-    public static boolean Debug = true;
+    public static boolean Debug = false;
     public static void LOG(String log)
     {
         if (Debug)
